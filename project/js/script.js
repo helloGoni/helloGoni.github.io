777fd1422c9a1b696212910e3a5406368eb28af7
helloGoni <88478753+helloGoni@users.noreply.github.com>
2021-08-16 16:17:35 +0900

    Add files via upload

---
+++ project/js/script.js
@@ -0,0 +1,96 @@
+
+function init() {
+  ballX = 600;
+  ballY = 340;
+  ballSize = 5;
+  ballSpeed = 1;
+  ctx = document.getElementById("gameCanvas").getContext("2d");
+}
+
+var ctx;
+var ballX;
+var ballY;
+var ballSize;
+var ballSpeed; // 공 관련 변수
+var batter1 = new Image();
+batter1.src = "../img/batter1.png"; // 타자 대기이미지 변수
+var batter2 = new Image();
+batter2.src;
+var pitcher = new Image();
+pitcher.src="../img/pitcher.png"; //투수 대기이미지 변수
+var score = 0;
+var outCount;
+var gamePlay = false;
+var pressed = false;
+
+
+function draw() {
+
+  ctx.clearRect(0,0,700,400);
+  drawBatter();
+  drawPitcher();
+  if (gamePlay==true) {
+    drawBall();
+    update();
+  }
+}
+
+function drawBatter() {
+  ctx.drawImage(batter1,30,300,50,100);
+}
+
+
+function drawPitcher() {
+  ctx.drawImage(pitcher,600,330,80,76);
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ballX,ballY,ballSize,0, Math.PI*2, true);
+  ctx.fillStyle="red";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function update() {
+  ballX -= ballSpeed;
+  if(ballX < 0 )
+  {
+    outCount--;
+  }
+}
+
+function gameStart() {
+  outCount = 3;
+  gamePlay = true;
+  window.requestAnimationFrame(game);
+  if(outCount = 0) {
+    gamePlay = false;
+  }
+}
+
+draw();
+
+
+
+document.addEventListener("keydown".keyDownHandler,false);
+document.addEventListener("keyup".keyUpHandler,false);
+function keyDownHandler(e) {
+  if(e.keyCode==32) {
+    pressed = true;
+  }
+}
+function keyUpHandler(e) {
+  if(e.keyCode == 32){
+    pressed = false;
+  }
+}
+
+
+function game(){
+  draw();
+
+  window.requestAnimationFrame(game);
+}
+
+
