26fa3c0ba7831fffccc9963034d870bed5377b19
helloGoni <88478753+helloGoni@users.noreply.github.com>
2021-08-26 15:49:43 +0900

    Add files via upload

--- project/js/script.js
+++ project/js/script.js
@@ -7,18 +7,27 @@ var ballSize;
 var ballSpeedX; // 공 관련 변수
 var ballSpeedY;
 var audio = new Audio('../img/ggang.mp3');
-audio.volume = 0.1;
+audio.volume = 0.15;
+
+var audio2 = new Audio('../img/wow.mp3');
+audio2.volume = 0.06;
+
+var backgroundImage = new Image();
+backgroundImage.src = "../img/background.jpg";
+
 var batter1 = new Image();
 batter1.src = "../img/batter1.png"; // 타자 대기이미지 변수
 var batter2 = new Image();
-batter2.src = "../img/batter2.png";
+batter2.src = "../img/batter2.png"; //타자 휘두르는중!
+var batter3 = new Image();
+batter3.src = "../img/batter3.png"; //타자 휘두르기 끝남!
 var pitcher = new Image();
 pitcher.src="../img/pitcher.png"; //투수 대기이미지 변수
 var score = 0;
 var outCount;
 var gamePlay = false;
 var pressed = false;
-var swing = false;
+var swing = 0;
 function init() {
   ballX = 600;
   ballY = 340;
@@ -30,6 +39,7 @@ function init() {
 
 function draw() {
   ctx.clearRect(0,0,700,400);
+  drawBackground();
   drawBatter();
   drawPitcher();
   drawBall();
@@ -37,9 +47,17 @@ function draw() {
 
   drawScore();
 }
+function drawBackground() {
+  ctx.save();
+  ctx.globalAlpha = 0.4;
+  ctx.drawImage(backgroundImage,0,0);
+  ctx.restore();
 
+}
 function drawBatter() {
-  (pressed?ctx.drawImage(batter2,30,310,80,100):ctx.drawImage(batter1,30,300,50,100));
+  (pressed?
+    ctx.drawImage(batter2,30,310,80,100)
+    :ctx.drawImage(batter1,30,300,50,100));
 }
 
 function drawPitcher() {
@@ -100,14 +118,19 @@ function game(){
   if (ballX<0 || ballX>700 || ballY < 0 )
   { 
     if (ballX<0) { outCount--; ballSpeedY=0;}    // 못 쳤음
-    if (ballX>700) { ballSpeedX = (-ballSpeedX); ballSpeedX++; score++; ballSpeedY=0;}
+
+    if (ballX>700) { ballSpeedX = (-ballSpeedX); ballSpeedX++; score++; ballSpeedY=0; //
+      if(ballY<30) { score++; audio2.play();} //홈런치면 스코어 3점
+      if(ballY<100) { score++; } //약간 더 밑이면 스코어 +2점
+      if(ballY<200) { score++;} //약간 더 밑이면 스코어 +1점
+    }
     ballX=600; ballY=340;   
     level++;
     var random = Math.floor(Math.random() * level*0.5) +2;
     ballSpeedX = random;
     ballSpeedY = 0;
   }
-  
+  //쳤는지 확인!
   if(pressed && ballX> 60 && ballX<90 && ballSpeedX>0 && ballY>325 && ballY<355){
     ballSpeedX = -(ballSpeedX+5);
     var random1 = Math.floor(Math.random()*5)+3;
